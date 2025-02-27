6764d98983387040c4f7dbda267e3a9c7442b7df
sofiainchausti <sofiabeleninchausti@gmail.com>
2025-02-27 16:20:26 -0300

    add prettier and lint to build command

--- themes/final-capstone-project/eslint.config.js
+++ themes/final-capstone-project/eslint.config.js
@@ -2,21 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],    
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     ignores: ["**/*.php"],
   },
 
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
-    }
+    },
   },
-  
 ];
